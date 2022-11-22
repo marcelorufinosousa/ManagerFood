551122ac79c98a07c4d542cc82df483f77dd9633
Marcelo Rufino <marcelorufinoo@gmail.com>
2022-11-21 22:07:34 -0300

    primeiro commit

---
+++ managerfood/managerfood/src/main/java/br/ztech/managerfood/ManagerfoodApplication.java
@@ -0,0 +1,13 @@
+package br.ztech.managerfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerfoodApplication.class, args);
+	}
+
+}
---
+++ managerfood/managerfood/src/test/java/br/ztech/managerfood/ManagerfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ztech.managerfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
